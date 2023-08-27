326cb11ab36bba5640f884d1cdde054456ec6ab1
Ren <rennguyen47@gmail.com>
2023-08-27 22:11:29 +1000

    -Cleaned up the code

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,9 @@
 #define SPI_SCK 13
 
 LSM6DSV16XSensor AccGyr(&SPI, CS_PIN);
-volatile uint8_t fullFlag = 0; // FIFO full flagn 
 uint8_t status = 0;
-unsigned long timestamp_count = 0;
-bool acc_available = false;
-bool gyr_available = false;
-int32_t acc_value[3];
-int32_t gyr_value[3];
-char buff[FLASH_BUFF_LEN];
-uint32_t pos = 0;
 
-void prereqSetup(uint8_t dataRSet, uint8_t gyroFiltS, uint8_t accFiltS, uint8_t xRange);
+void prereqSetup(uint8_t dataRSet, uint8_t gyroFiltS, uint8_t accFiltS, uint8_t xRange, uint8_t gRangeS);
 void regularSetup(int dataRSet);
 void gameSetup(int gameODRSet);
 
@@ -85,22 +77,32 @@ const uint8_t accRange[] = {  //the comments below represent the range and decim
   3   //+16g, 2048
 };
 
+const uint8_t gyrRange[] = {  //the comments below represent the range (in dps)
+  0b0000, //125
+  0b0001, //250
+  0b0010, //500
+  0b0011, //1000
+  0b0100, //2000
+  0b1100  //4000
+};
+
 void setup() {
   uint8_t dRSet = 7;    //gyr and acc output data rate choice (refer to dR[])
   uint8_t gODRSet = 3;  //game vector output data rate choice (refer to gameODR[])
   uint8_t gFiltSetting = 4, xFiltSetting = 5; //gyro and acc filter bandwidth selection (refer to gF_LU240Hz[] and aF_LU_HP[])
-  uint8_t xRange = 0; //acc and gyro range (refer to accRange[] and)
+  uint8_t xRangeS = 1, gRangeS = 2; //acc and gyro range setting (refer to accRange[] and gyrRange[])
 
   Serial.begin(115200);
   SPI.begin();
 
-  prereqSetup(dRSet, gFiltSetting, xFiltSetting, xRange);
+  prereqSetup(dRSet, gFiltSetting, xFiltSetting, xRangeS, gRangeS);
   
-  //gameSetup(gODRSet);
-  regularSetup(dRSet);
+  gameSetup(gODRSet);
+  //regularSetup(dRSet);
 }
 
-void prereqSetup(uint8_t dataRSet, uint8_t gyroFiltS, uint8_t accFiltS, uint8_t xRange){ //setup required for both game vector or acc & gyro only mode
+void prereqSetup(uint8_t dataRSet, uint8_t gyroFiltS, uint8_t accFiltS, uint8_t xRangeS,
+uint8_t gRangeS){ //setup required for both game vector or acc & gyro only mode
   // Initialize LSM6DSV16X.
   AccGyr.begin();
 
@@ -112,12 +114,11 @@ void prereqSetup(uint8_t dataRSet, uint8_t gyroFiltS, uint8_t accFiltS, uint8_t 
 
   // Configure FS of the acc and gyro
   AccGyr.Write_Reg(0x01, 0b00000000 + 0b00000000); //disable the embed reg access
-  status |= AccGyr.Set_G_FS(GYR_FS);
 
   // Setting the filters and scale
-  AccGyr.Write_Reg(0x18, (1<<5) + (0<<4)); //turning on fast settling mode and selecting high pass for accelereometer
-  AccGyr.Write_Reg(0x17, accRange[xRange] + (accFiltS<<5));  //setting the scale to +-2g and bandwidth
-  AccGyr.Write_Reg(0x15, (gF_LU240Hz[gyroFiltS]<<4));  //setting the gyroscope lp filter bandwidth
+  AccGyr.Write_Reg(0x18, (1<<5) + (0<<4)); //turning on fast settling mode and selecting low pass for accelereometer
+  AccGyr.Write_Reg(0x17, accRange[xRangeS] + (accFiltS<<5));  //setting the scale to +-2g and bandwidth
+  AccGyr.Write_Reg(0x15, (gF_LU240Hz[gyroFiltS]<<4) + gyrRange[gRangeS]);  //setting the gyroscope lp filter bandwidth and setting the scale
 }
 
 void regularSetup(int dataRSet){  //to setup only the acc and gyr
@@ -183,35 +184,20 @@ void unityDataPrep(int16_t gameArr[]){
 }
 
 void getFifoData(int16_t quatData[]){
-  uint8_t fifoData[3], tag, temp;;
-  uint16_t temp16;
-  float quatVal[3];
-  
+  uint8_t temp[6], startAddr = 0x79;
   
   AccGyr.Write_Reg(0x01, 0b00000000 + 0b00000000); //disable the embed reg access
-  //AccGyr.FIFO_Get_Tag(&tag);  Serial.print("Tag: "); Serial.println(tag, HEX);
-  //AccGyr.FIFO_Get_Full_Status(&temp);  Serial.print("Status: "); Serial.println(temp);
-  //AccGyr.FIFO_Get_Num_Samples(&temp16); Serial.print("Num of Samples: "); Serial.println(temp16);
-  AccGyr.FIFO_Get_Data(fifoData); //Serial.println("Game Vector: ");   //for some reason this command causes the fifo tag and num of samples to be 0
+  
 
-  for(int i = 0; i < 3; i++)
+  for(uint8_t j = 0; j < 3; j++)
   {
-     quatData[i] = (int16_t)(fifoData[i*2] << 8) + (int16_t)fifoData[i*2+1];
-     //quatVal[i] = quatData/65536;
-     //quatData[i] = (fifoData[i*2]) + (int16_t)(fifoData[i*2+1] << 8);
-     //Serial.print("Gamedata No. ");  Serial.print(i); Serial.print(" :"); Serial.println(quartData[i]);
+    for(uint8_t i = 0; i < 6; i ++)
+    {
+      AccGyr.Read_Reg(startAddr + i, &temp[i]);
+    }
+    quatData[j] = (uint16_t)(temp[j*2+1]<<8) + (uint16_t)temp[j*2];
   }
-  Serial.print("Quat X: "); Serial.print(quatData[0]); Serial.print(" Quat Y: "); Serial.print(quatData[1]); Serial.print(" Quat Z: "); Serial.println(quatData[2]); 
-  //Serial.print("quatData[0] (byte check):"); Serial.println(quatData[0],BIN); Serial.print("quatData[0] (hex check):"); Serial.println(quatData[0],HEX);
-
-  //Serial.print("X: "); Serial.println(quatData[0]);
-}
-
-void get_AccGyro(uint8_t gameData[]){
-  int32_t gyroVal[3], accVal[3];
-
-  AccGyr.FIFO_Get_X_Axes(accVal); Serial.print(" AccX:"); Serial.print(accVal[0]);  Serial.print(" AccY:"); Serial.print(accVal[1]);  Serial.print(" AccZ:"); Serial.println(accVal[2]);
-  //AccGyr.FIFO_Get_G_Axes(gyroVal); Serial.print(" GyrX:");  Serial.print(gyroVal[0]); Serial.print(" GyrY:");  Serial.print(gyroVal[1]); Serial.print(" GyrZ:");  Serial.println(gyroVal[2]);
+  //Serial.print("Quat X: "); Serial.print(quatData[0]); Serial.print(" Quat Y: "); Serial.print(quatData[1]); Serial.print(" Quat Z: "); Serial.println(quatData[2]); 
 }
 
 void getAccRaw(){
@@ -225,21 +211,16 @@ void getAccRaw(){
       AccGyr.Read_Reg(startAddr + i, &temp[i]);
     }
 
-
-    //accData[0] = (uint16_t)(temp[1]<<8) + (uint16_t)temp[0];  //x axis
-    //accData[1] = (uint16_t)(temp[3]<<8) + (uint16_t)temp[2];  //y axis
-    //accData[2] = (uint16_t)(temp[5]<<8) + (uint16_t)temp[4];  //z axis
-
     accData[j] = (uint16_t)(temp[j*2+1]<<8) + (uint16_t)temp[j*2];
     
   }
   //accData[0] = (uint16_t)(temp[1]<<8) + (uint16_t)temp[0];  Serial.print("Acc Z: ");  Serial.println(accData[0]);
-  Serial.print("Acc X: ");  Serial.print(accData[0]); Serial.print(" Acc Y: ");  Serial.print(accData[1]);  Serial.print(" Acc Z: ");  Serial.println(accData[2]);     
+  Serial.print("Acc X: ");  Serial.print(accData[0]); Serial.print(" Acc Y: ");  Serial.print(accData[1]);  Serial.print(" Acc Z: ");  Serial.print(accData[2]);     
 }
 
 void getGyrRaw(){
   uint8_t temp[6], startAddr = 0x22;
-  int16_t accData[3];
+  int16_t gyrData[3];
   
   for(uint8_t j = 0; j < 3; j++)
   {
@@ -247,17 +228,11 @@ void getGyrRaw(){
     {
       AccGyr.Read_Reg(startAddr + i, &temp[i]);
     }
-
-
-    //accData[0] = (uint16_t)(temp[1]<<8) + (uint16_t)temp[0];  //x axis
-    //accData[1] = (uint16_t)(temp[3]<<8) + (uint16_t)temp[2];  //y axis
-    //accData[2] = (uint16_t)(temp[5]<<8) + (uint16_t)temp[4];  //z axis
-
-    accData[j] = (uint16_t)(temp[j*2+1]<<8) + (uint16_t)temp[j*2];
+    gyrData[j] = (uint16_t)(temp[j*2+1]<<8) + (uint16_t)temp[j*2];
     
   }
   //accData[0] = (uint16_t)(temp[1]<<8) + (uint16_t)temp[0];  Serial.print("Acc Z: ");  Serial.println(accData[0]);
-  Serial.print("Gyr X: ");  Serial.print(accData[0]); Serial.print(" Gyr Y: ");  Serial.print(accData[1]);  Serial.print(" Gyr Z: ");  Serial.println(accData[2]);     
+  Serial.print("     Gyr X: ");  Serial.print(gyrData[0]); Serial.print("   Gyr Y: ");  Serial.print(gyrData[1]);  Serial.print("   Gyr Z: ");  Serial.println(gyrData[2]);     
 }
 
 void quatToEuler(int16_t q[]){
@@ -268,7 +243,6 @@ void quatToEuler(int16_t q[]){
   q[3] = 1;
   //Roll calculation
   eulerResult[0] = atan2( ( 2*(q[0]*q[1] + (q[2]*q[3]) ) ) , (sq(q[0]) + sq(q[3]) - sq(q[1]) - sq(q[2])) ); //you can't square numbers like this "x^2", this is just "x+2" in c
-  eulerDegrees[0] = (float)eulerResult[0] * (float)(180/3.14);  //converting from radians to degrees
 
   //Pitch calculation
   eulerResult[1] =  asin(    2*((q[0]*q[2])-(q[1]*q[3])) );
@@ -276,8 +250,14 @@ void quatToEuler(int16_t q[]){
   //Yaw calculation
   eulerResult[2] =  atan2(   (2*(q[0]*q[3]+(q[1]*q[2]))) , (sq(q[0])+sq(q[1])-sq(q[2])-sq(q[3])) );
 
+  //Conversion to degrees
+  for(uint8_t i = 0; i < 3; i++)
+  {
+    eulerDegrees[i] = (float)eulerResult[0] * (float)(180/3.14);  
+  }
+
   Serial.print("Roll (radians):"); Serial.print(eulerResult[0]);  Serial.print(" Pitch (radians):"); Serial.print(eulerResult[1]); Serial.print(" Yaw (radians):"); Serial.println(eulerResult[2]);
-  //Serial.print("Euler ang x (degrees):"); Serial.println(eulerDegrees[0]);
+  //Serial.print("Roll (degrees):"); Serial.print(eulerDegrees[0]);  Serial.print(" Pitch (degrees):"); Serial.print(eulerDegrees[1]); Serial.print(" Yaw (degrees):"); Serial.println(eulerDegrees[2]);
 }
 
 void loop() {
@@ -288,18 +268,16 @@ void loop() {
 
   //===Functions for game====
   //
-  //getFifoData(quat);
+  getFifoData(quat);
   //unityDataPrep(quat);
-  //quatToEuler(quat);
+  quatToEuler(quat);
   //delay(1);
   //
   //=========================
 
   //===Functions for AccGyro only======
   //
-  //get_AccGyro(gameData);
-  getAccRaw();
-  //getGyrRaw();
+  //getAccRaw();  getGyrRaw();
   //delay(1);
   //
 }
