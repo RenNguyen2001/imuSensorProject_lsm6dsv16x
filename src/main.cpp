59f57a08496f1393fa92c29cb6f77f379908650b
Ren <rennguyen47@gmail.com>
2023-10-16 18:12:50 +1100

    - Clean up a lot of the code. - Added structs to store the cs pin numbers, and finger names of each flex pcb strip.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <LSM6DSV16XSensor.h>
 #include <math.h>
+#include <SPI.h>
 
 #define SENSOR_ODR 120.0f // In Hertz
 #define ACC_FS 2 // In g
@@ -16,6 +17,11 @@ uint8_t CS_PIN  = 17;
 #define SPI_MISO  11
 #define SPI_SCK 13
 
+#define chipSelect 4 //BO_00 pin name
+#define readSensor 1
+#define writeSensor 0
+SPISettings settingsA(1000000, MSBFIRST, SPI_MODE1);
+
 LSM6DSV16XSensor AccGyr(&SPI, CS_PIN);
 uint8_t status = 0;
 int16_t firstFrame[3];
@@ -25,6 +31,11 @@ void prereqSetup(uint8_t dataRSet, uint8_t gyroFiltS, uint8_t accFiltS, uint8_t 
 void regularSetup(int dataRSet);
 void gameSetup(int gameODRSet);
 void getFifoData(int16_t quatData[], int chipS_Pin);
+
+void chipSelectSetup();
+void SPItransmit(char rw, char address, char spiData);
+void IMU_setup();
+void IMU_read();
 
 //const arrays to store values
 
@@ -98,11 +109,32 @@ const uint8_t gyrRange[] = {  //the comments below represent the range (in dps)
   0b1100  //4000
 };
 
-const uint8_t indexIMUs[] = {
-  17,
-  18,
-  19
-};
+typedef struct{
+  const uint8_t imuCS_pins[3];
+  const String fingerName; 
+}imuDetails;
+
+//CS1 is the tip of the finger, CS2 is the middle, CS3 is the base
+
+imuDetails thumb = {{38,36,37},"Thumb: "};
+imuDetails indexFinger = {{27,29,28},"Index: "};
+imuDetails middle = {{8,10,9},"Middle: "};
+imuDetails ring = {{2,4,3},"Ring: "};
+imuDetails pinkie = {{17,0,16},"Pinkie: "};
+
+imuDetails imuArr[] = {thumb,indexFinger,middle,ring,pinkie};
+
+typedef struct jointAngle{
+  const uint8_t adjacentIMU_cs[2]; //the two IMUs that form the specific joint
+  const String jointName;
+  uint16_t angle;
+}jointAngle;
+
+jointAngle thumbDIP = {{thumb.imuCS_pins[0],thumb.imuCS_pins[1]}, "thumb DIP", 0};
+jointAngle thumbPIP = {{thumb.imuCS_pins[1],thumb.imuCS_pins[2]}, "thumb PIP", 0};
+jointAngle indexDIP = {{indexFinger.imuCS_pins[0],indexFinger.imuCS_pins[1]}, "index DIP", 0};
+jointAngle indexPIP = {{indexFinger.imuCS_pins[1],indexFinger.imuCS_pins[2]}, "index PIP", 0};
+
 
 void setup() {
   uint8_t dRSet = 7;    //gyr and acc output data rate choice (refer to dR[])
@@ -116,23 +148,83 @@ void setup() {
   Serial.begin(120000);
   SPI.begin();
 
-  Serial.println("Setting up systems....");
+  //chipSelectSetup();
 
-  for(char i = 0; i <= sizeof(indexIMUs); i++)
+  Serial.println("Setting up systems....");
+  
+
+  for(char i = 0; i < 3; i++)
   {
-    AccGyr.cs_pin = indexIMUs[i]; //changing the cs_pin from within the header file, made the cs_pin variable public
+    AccGyr.cs_pin = thumb.imuCS_pins[i]; //changing the cs_pin from within the header file, made the cs_pin variable public
     prereqSetup(dRSet, gFiltSetting, xFiltSetting, xRangeS, gRangeS, csPin);
     
     gameSetup(gODRSet); //setup game
-    delay(1);
+    delay(5);
   }
-  
+
+  for(char i = 0; i < 3; i++)
+  {
+    AccGyr.cs_pin = indexFinger.imuCS_pins[i]; //changing the cs_pin from within the header file, made the cs_pin variable public
+    prereqSetup(dRSet, gFiltSetting, xFiltSetting, xRangeS, gRangeS, csPin);
+    
+    gameSetup(gODRSet); //setup game
+    delay(5);
+  }
+
+  for(char i = 0; i < 3; i++)
+  {
+    AccGyr.cs_pin = middle.imuCS_pins[i]; //changing the cs_pin from within the header file, made the cs_pin variable public
+    prereqSetup(dRSet, gFiltSetting, xFiltSetting, xRangeS, gRangeS, csPin);
+    
+    gameSetup(gODRSet); //setup game
+    delay(5);
+  }
+
+  for(char i = 0; i < 3; i++)
+  {
+    AccGyr.cs_pin = ring.imuCS_pins[i]; //changing the cs_pin from within the header file, made the cs_pin variable public
+    prereqSetup(dRSet, gFiltSetting, xFiltSetting, xRangeS, gRangeS, csPin);
+    
+    gameSetup(gODRSet); //setup game
+    delay(5);
+  }
+
+  for(char i = 0; i < 3; i++)
+  {
+    AccGyr.cs_pin = pinkie.imuCS_pins[i]; //changing the cs_pin from within the header file, made the cs_pin variable public
+    prereqSetup(dRSet, gFiltSetting, xFiltSetting, xRangeS, gRangeS, csPin);
+    
+    gameSetup(gODRSet); //setup game
+    delay(5);
+  }
+}
+
+void chipSelectSetup(){ //see pages 948 and 957 in the manual
+  // 1. Set pads to GPIO mode using IOMUX registers 
+  //find your pin in the schematic, pin 13 was named as B0_03/led pin
+    //IOMUXC_SW_MUX_CTL_PAD_GPIO_B0_03 = 5; 
+    IOMUXC_SW_MUX_CTL_PAD_GPIO_EMC_04 = 5; //GPIO4
+
+  // 2. Setting GPIO as standard mode instead of high speed
+  //e.g. regular and high speed GPIO are shared, IOMUXC_GPR is used to determine if regular or
+  //high speed is selected
+  //IOMUXC_GPR_GPR26-29 determine what mode is selected
+    //IOMUXC_GPR_GPR27 = 0x00000000;
+    IOMUXC_GPR_GPR29 = 0x00;
+
+  // 3. Setting the data direction register (make IO an output)
+    //GPIO2_GDIR |= (1 << chipSelect);
+    GPIO4_GDIR |= (1 << chipSelect);
 }
 
 void prereqSetup(uint8_t dataRSet, uint8_t gyroFiltS, uint8_t accFiltS, uint8_t xRangeS,
 uint8_t gRangeS, uint8_t csPin){ //setup required for both game vector or acc & gyro only mode
   // Initialize LSM6DSV16X.
   AccGyr.begin();
+
+  //AccGyr.Write_Reg(0x03, 0b1);  //disable i2c
+  AccGyr.Write_Reg(0x03, 0b1 + (0b1<<7)); //disable I2C and I3C, and enable the pull up on sda
+  AccGyr.Write_Reg(0x02, 0b1<<6); //enabling the pull up resistor on the MISO line
 
   AccGyr.Write_Reg(0x10, dR[dataRSet] + 0b00010000); //enable acc by setting the odr and setting to high acc ODR mode
   AccGyr.Write_Reg(0x11, dR[dataRSet] + 0b00010000); //enable gyro by setting the odr and setting to high acc ODR mode
@@ -215,21 +307,20 @@ void unityDataPrep(int16_t gameArr[]){
 
 void getFifoData(int16_t quatData[], int chipS_Pin){
   AccGyr.cs_pin = chipS_Pin;
-  delayMicroseconds(3);
+  delayMicroseconds(100);
   uint8_t temp[6], startAddr = 0x79;
   float quatFloat[3];
   
   AccGyr.Write_Reg(0x01, 0b00000000 + 0b00000000); //disable the embed reg access
   
-
-  for(uint8_t j = 0; j < 3; j++)
+  for(uint8_t i = 0; i < 6; i++)
   {
-    for(uint8_t i = 0; i < 6; i++)
-    {
-      AccGyr.Read_Reg(startAddr + i, &temp[i]);
-    }
-    quatData[j] = (uint16_t)(temp[j*2+1]<<8) + (uint16_t)temp[j*2];
-    //quatFloat[j] = (float)quatData[j]/65536.0;
+    AccGyr.Read_Reg(startAddr + i, &temp[i]);
+  }
+    
+  for(uint8_t i = 0; i < 3; i++)
+  {
+    quatData[i] = (uint16_t)(temp[i*2+1]<<8) + (uint16_t)temp[i*2];
   }
   //Serial.print("Quat X: "); Serial.print(quatData[0]); Serial.print(" Quat Y: "); Serial.print(quatData[1]); Serial.print(" Quat Z: "); Serial.println(quatData[2]);
   //Serial.print(quatData[0]); Serial.print("  "); Serial.print(quatData[1]); Serial.print("  "); Serial.println(quatData[2]); 
@@ -277,7 +368,7 @@ float calculate3DVecAngle(float vecA[], float vecB[]){ //calculates the angle in
   return degrees;
 }
 
-void gravityVecToEuler(int16_t quatData[], uint8_t index, float output[][2]){ //uses the gravity vector to calculate euler angles
+void gravityVecToEuler(int16_t quatData[], uint8_t indexNo, float output[][2]){ //uses the gravity vector to calculate euler angles
   float gravValue = 16384;
   float pitchTheta, rollTheta;
   float zAxisVec[3] = {0,gravValue};
@@ -293,9 +384,7 @@ void gravityVecToEuler(int16_t quatData[], uint8_t index, float output[][2]){ //
   //calculating roll (rotation around y axis/ change across the x and z axis)
   rollTheta = calculate3DVecAngle(gravVectorRoll, zAxisVec);
 
-  output[index][0] = pitchTheta;  output[index][1] = rollTheta;
-
-  //Serial.print(" Pitch angle: ");  Serial.print(pitchTheta); Serial.print(" Roll angle: ");  Serial.println(rollTheta); Serial.println("");
+  output[indexNo][0] = pitchTheta;  output[indexNo][1] = rollTheta;
 }
 
 void calculateJointAng(float input[][2], uint8_t imu1, uint8_t imu2, float result[]){ //calculates the angle between two adjacent IMUs
@@ -325,34 +414,55 @@ void calculateJointAng(float input[][2], uint8_t imu1, uint8_t imu2, float resul
     }
     
   //Serial.print("Pitch D: ");  Serial.print(result[0]);  Serial.print(" Roll D: ");  Serial.println(result[1]);
-  Serial.print(imu1);  Serial.print(" ");  Serial.print(imu2);  Serial.print(" "); Serial.print(result[0]);  Serial.print(" ");  Serial.println(result[1]);
+  //Serial.print(imu1);  Serial.print(" ");  Serial.print(imu2);  Serial.print(" "); Serial.print(result[0]);  Serial.print(" ");  Serial.println(result[1]);
 }
 
 
 
-void loop() {
-  uint8_t gameData[6];
-  int16_t eulerAng[3], fifoOut[4];
-  float outVals[32][2], jointAng[2];
-
-  //checkGameRegs();
-
-  //===Functions for game====
-  //
-  for(int i = 0; i < indexIMUs[i]; i++)
+void readSingleIMUstrip(int16_t fifoOut[4], float outVals[][2], const uint8_t fingerNum){ //0 for thumb... 4 for pinkie
+  for(int i = 0; i < 3; i++)
   {
-    //Serial.print("CS: "); Serial.println(i, DEC);
-    getFifoData(fifoOut, indexIMUs[i]);  gravityVecToEuler(fifoOut, indexIMUs[i], outVals);
+    //Serial.print(imuArr[fingerNum].fingerName); Serial.print(imuArr[fingerNum].imuCS_pins[i], DEC); Serial.print("  ");
+    getFifoData(fifoOut, imuArr[fingerNum].imuCS_pins[i]);  gravityVecToEuler(fifoOut, imuArr[fingerNum].imuCS_pins[i], outVals);
+    //Serial.print(fifoOut[0]); Serial.print("  "); Serial.print(fifoOut[1]); Serial.print("  "); Serial.println(fifoOut[2]);
   }
-  calculateJointAng(outVals, indexIMUs[0], indexIMUs[0+1], jointAng);
-  //calculateJointAng(outVals, 18, 19, jointAng);
+}
 
-  //unityDataPrep(quat);
-  //quatToEuler(quat);
-  //quat2EulerManual(quat);
-  //delay(1);
-  //
-  //=========================
+void readSingleIMU(int16_t fifoOut[4], float outVals[][2], const uint8_t fingerNum, const uint8_t imuNum){
+    Serial.print(imuArr[fingerNum].fingerName); Serial.print(imuArr[fingerNum].imuCS_pins[imuNum], DEC); Serial.print("  ");
+    getFifoData(fifoOut, imuArr[fingerNum].imuCS_pins[imuNum]);  gravityVecToEuler(fifoOut, imuArr[fingerNum].imuCS_pins[imuNum], outVals);
+    //Serial.print(fifoOut[0]); Serial.print("  "); Serial.print(fifoOut[1]); Serial.print("  "); Serial.println(fifoOut[2]); //reading the raw gravity vector values
+    Serial.print(" Pitch angle: ");  Serial.print(outVals[imuArr[fingerNum].imuCS_pins[imuNum]][0]); Serial.print(" Roll angle: ");  Serial.println(outVals[imuArr[fingerNum].imuCS_pins[imuNum]][1]); Serial.println("");
+
+}
+
+
+void readMultiIMUstrip(int16_t fifoOut[4], float outVals[][2]){
+  readSingleIMUstrip(fifoOut, outVals, 1);
+  readSingleIMUstrip(fifoOut, outVals, 0);
+  //readSingleIMUstrip(fifoOut, outVals, 2);
+  //readSingleIMUstrip(fifoOut, outVals, 3);
+  //readSingleIMUstrip(fifoOut, outVals, 4);
+}
+
+void readIMUstripJoint(int16_t fifoOut[4], float outVals[][2], float jointAng[], const uint8_t fingerNum){
+  readSingleIMUstrip(fifoOut, outVals, fingerNum);
+  calculateJointAng(outVals, indexDIP.adjacentIMU_cs[0], indexDIP.adjacentIMU_cs[1], jointAng);
+  Serial.print("Joint angle: ");  Serial.print(" "); Serial.println((String)jointAng[0]);
+  //calculateJointAng(outVals, 18, 19, jointAng);
+}
+
+void loop() {
+  int16_t fifoOut[4];
+  float outVals[40][2], jointAng[2];  //outvals is to store pitch and roll
+  
+
+  readIMUstripJoint(fifoOut, outVals, jointAng, 1);
+  //readSingleIMUstrip(fifoOut, outVals, 1); 
+  //readSingleIMU(fifoOut, outVals, 1, 0);
+  //readMultiIMUstrip(fifoOut, outVals);
+
+  //readIMU_manual();
 
   //===Functions for AccGyro only======
   //
@@ -360,6 +470,8 @@ void loop() {
   //delay(1);
   //
 }
+
+
 
 void checkGameRegs(){
   uint8_t tag, temp;
